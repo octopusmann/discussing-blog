66c9b821de50a37f91a2f75b1533c98cc64a1876
octopusmann <patricksdrama@gmail.com>
2024-12-22 22:42:03 +0400

    packages and database were set up

---
+++ src/app/clientComponent.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { Session } from "next-auth";
+
+interface ClientComponentProps {
+  session: Session | null;
+}
+
+export default function ClientComponent({ session }: ClientComponentProps) {
+  // Handle forms
+  const handleSignIn = async () => {
+    await actions.signIn();
+  };
+
+  const handleSignOut = async () => {
+    await actions.signOut();
+  };
+
+  return (
+    <div>
+      <form action={handleSignIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={handleSignOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
+    </div>
+  );
+}
